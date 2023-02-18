1cdaa858efaf22e10708e95319d9138f366cde9c
Grant Lemons <grant0lemons@gmail.com>
2023-02-18 11:42:19 -0600

    structure lib file structure

---
+++ src/lib/components/position.rs
@@ -0,0 +1,5 @@
+#[derive(Default, Debug)]
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
---
+++ src/lib/lib.rs
@@ -0,0 +1,6 @@
+pub mod components {
+    mod position;
+    pub use position::Position;
+}
+pub mod entities {}
+pub mod systems {}
